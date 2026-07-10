a52058e7b4c8d81e5aeedd3f27dc62e256406d1a
Eziorobo John Ezeakpono <eziorobojohn@gmail.com>
2026-07-10 11:36:32 +0100

    Update EC2 instance type to m7i-flex.large

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -1,6 +1,6 @@
 resource "aws_instance" "web_server" {
   ami                         = "ami-05ffe3c48a9991133"
-  instance_type               = var.instance_type
+  instance_type               = "m7i-flex.large"
   subnet_id                   = aws_subnet.public.id
   vpc_security_group_ids      = [aws_security_group.web_sg.id]
   associate_public_ip_address = true
